a341e3eb49d444246901bb9a15a2f4e64a8a4d07
Sam McHardy <hello@sammchardy.net>
2018-10-17 20:11:54 +1100

    Update version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='aquasystems-driver',
-    version='0.0.1',
+    version='0.0.2',
     packages=['aquasystems'],
     description='MQTT Bluetooth Service for Aqua Systems Tap Timer',
     url='https://github.com/sammchardy/aquasystems-driver',
